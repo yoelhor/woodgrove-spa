02b2925802c90ced6459ce405a3c5f550ee229f8
Yoel Horvitz <yoelhor@live.com>
2024-08-05 09:00:49 +0300

    Adding bootstrap

--- src/routes/Layout.jsx
+++ src/routes/Layout.jsx
@@ -2,22 +2,37 @@ import { Outlet, Link } from "react-router-dom";
 
 const Layout = () => {
     return <>
-        <div id="sidebar">
-            {/* other elements */}
+        <header className="header">
+            <div className="container">
+                {/* other elements */}
 
-            <nav>
-                <ul>
-                    <li>
-                        <Link to={`token`}>Token</Link>
-                    </li>
-                    <li>
-                        <Link to={`profile`}>Profile</Link>
-                    </li>
-                </ul>
-            </nav>
-        </div>
+                <nav className="navbar navbar-expand-lg bg-body-tertiary">
+                    <div className="container-fluid">
+                        <Link to={`token`} className="navbar-brand">Woodgrove home</Link>
 
-        <div id="detail">
+                        <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
+                            data-bs-target="#navbarContent" aria-controls="navbarContent" aria-expanded="false"
+                            aria-label="Toggle navigation">
+                            <span className="navbar-toggler-icon"></span>
+                        </button>
+
+                        <div className="collapse navbar-collapse" id="navbarContent">
+                            <div className="navbar-nav me-auto mb-2"> </div>
+                            <ul className="navbar-nav">
+                                <li className="nav-item">
+                                    <Link to={`token`} className="nav-link header-button">Token</Link>
+                                </li>
+                                <li className="nav-item">
+                                    <Link to={`profile`} className="nav-link header-button">Profile</Link>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </nav>
+            </div>
+        </header>
+
+        <div id="mainContent">
             <Outlet />
         </div>
     </>;
